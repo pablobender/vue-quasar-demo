ed3dc260c21d3531b7a4414165e1c50c8ce3d67b
Pablo Bender <pablo@bender.eti.br>
2019-03-13 15:07:36 -0400

    vue invoke quasar

--- src/main.js
+++ src/main.js
@@ -1,6 +1,51 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import './styles/quasar.styl'
+import lang from 'quasar/lang/pt-br.js'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import {
+  Quasar, 
+  QLayout,
+  QHeader,
+  QDrawer,
+  QPageContainer,
+  QPage,
+  QToolbar,
+  QToolbarTitle,
+  QBtn,
+  QIcon,
+  QList,
+  QItem,
+  QItemSection,
+  QItemLabel,
+} from 'quasar'
+
+Vue.use(Quasar, {
+  config: {},
+  components: {
+    QLayout,
+    QHeader,
+    QDrawer,
+    QPageContainer,
+    QPage,
+    QToolbar,
+    QToolbarTitle,
+    QBtn,
+    QIcon,
+    QList,
+    QItem,
+    QItemSection,
+    QItemLabel,
+  },
+  directives: {
+  },
+  plugins: {
+  },
+  lang: lang
+ })
+
 Vue.config.productionTip = false
 
 new Vue({
